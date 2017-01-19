e42dfe97f3b8bdf8767ff4fed20a0c752897e5eb
Travis Spire-Sweet <spiresweet@yahoo.com>
2017-01-19 02:28:49 -0600

    Aeon SOS button managing garage door opening

--- FirstProject.groovy
+++ FirstProject.groovy
@@ -1,1 +1,155 @@
-//first project
+/**
+ *  Smart GarageDoor Button
+ *
+ *  Copyright 2017 Travis Spire-Sweet
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+definition(
+    name: "Smart GarageDoor Button",
+    namespace: "godApinow",
+    author: "Travis Spire-Sweet",
+    description: "Converting Aeon sos button to smart garage door opener with appropriate alerts",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Button") {
+		input "button", "capability.button", title: "Button Pushed", required: false, multiple: true //tw
+	}
+	section("GarageDoor"){
+		input "contact", "capability.contactSensor", title: "Contact Opens", required: false, multiple: true
+		input "mySwitch", "capability.switch", title: "Switch Turned On", required: false, multiple: true	
+	}
+
+	section("Send this message"){
+		input "messageText", "text", title: "Message Text", required: false
+	}
+	section("Via a push notification and/or an SMS message"){
+		input("recipients", "contact", title: "Send notifications to") {
+			input "phone", "phone", title: "Enter a phone number to get SMS", required: false
+			paragraph "If outside the US please make sure to enter the proper country code"
+			input "pushAndPhone", "enum", title: "Notify me via Push Notification", required: false, options: ["Yes", "No"]
+		}
+	}
+	section("Minimum time between messages (optional, defaults to every message)") {
+		input "frequency", "decimal", title: "Minutes", required: false
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+	unsubscribe()
+	initialize()
+	//subscribeToEvents()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+    log.debug "initializing"
+    subscribe(button, "button.pushed", eventHandler)
+	subscribe(contact, "contact.open", eventHandler)
+    subscribe(contact, "contact.close", eventHandler)
+	subscribe(mySwitch, "switch.on", eventHandler)
+	subscribe(mySwitch, "switch.off", eventHandler)
+}
+
+def subscribeToEvents(){
+
+}
+
+def eventHandler(evt) {
+	log.debug "Notify got evt ${evt}"
+			if (mySwitch.switch=="off") {
+            log.debug "contact open"
+				mySwitch.off()
+                return;
+			} else {
+            log.debug "contact closed"
+				mySwitch.on()
+			}
+
+}
+
+
+private sendMessage(evt) {
+	String msg = messageText
+	Map options = [:]
+
+	if (!messageText) {
+		msg = defaultText(evt)
+		options = [translatable: true, triggerEvent: evt]
+	}
+	log.debug "$evt.name:$evt.value, pushAndPhone:$pushAndPhone, '$msg'"
+
+	if (location.contactBookEnabled) {
+		sendNotificationToContacts(msg, recipients, options)
+	} else {
+		if (phone) {
+			options.phone = phone
+			if (pushAndPhone != 'No') {
+				log.debug 'Sending push and SMS'
+				options.method = 'both'
+			} else {
+				log.debug 'Sending SMS'
+				options.method = 'phone'
+			}
+		} else if (pushAndPhone != 'No') {
+			log.debug 'Sending push'
+			options.method = 'push'
+		} else {
+			log.debug 'Sending nothing'
+			options.method = 'none'
+		}
+		sendNotification(msg, options)
+	}
+	if (frequency) {
+		state[evt.deviceId] = now()
+	}
+}
+
+private defaultText(evt) {
+	if (evt.name == 'presence') {
+		if (evt.value == 'present') {
+			if (includeArticle) {
+				'{{ triggerEvent.linkText }} has arrived at the {{ location.name }}'
+			}
+			else {
+				'{{ triggerEvent.linkText }} has arrived at {{ location.name }}'
+			}
+		} else {
+			if (includeArticle) {
+				'{{ triggerEvent.linkText }} has left the {{ location.name }}'
+			}
+			else {
+				'{{ triggerEvent.linkText }} has left {{ location.name }}'
+			}
+		}
+	} else {
+		'{{ triggerEvent.descriptionText }}'
+	}
+}
+
+
+private getIncludeArticle() {
+	def name = location.name.toLowerCase()
+	def segs = name.split(" ")
+	!(["work","home"].contains(name) || (segs.size() > 1 && (["the","my","a","an"].contains(segs[0]) || segs[0].endsWith("'s"))))
+}
